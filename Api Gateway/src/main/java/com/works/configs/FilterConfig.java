7e866a4f4d55366f3bcd5536df16196b521dbd71
Hakan ÖZER <hakanozer02@gmail.com>
2025-01-17 14:36:36 +0300

    Filter Config

---
+++ Api Gateway/src/main/java/com/works/configs/FilterConfig.java
@@ -0,0 +1,31 @@
+package com.works.configs;
+
+import io.micrometer.tracing.Tracer;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig implements Filter {
+
+    final Tracer tracer;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String spanId = tracer.currentSpan().context().spanId();
+        String traceId = tracer.currentSpan().context().traceId();
+        response.setHeader("spanId", spanId);
+        response.setHeader("traceId", traceId);
+
+        filterChain.doFilter(request, response);
+    }
+
+}
