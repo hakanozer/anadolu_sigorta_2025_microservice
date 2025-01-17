cc138ae11ea635b2c3ae07b02c944f5ba3b7cae4
Hakan ÖZER <hakanozer02@gmail.com>
2025-01-17 15:56:44 +0300

    JMS

--- Product/src/main/java/com/works/services/ProductService.java
+++ Product/src/main/java/com/works/services/ProductService.java
@@ -2,7 +2,11 @@ package com.works.services;
 
 import com.works.entities.Product;
 import com.works.repositories.ProductRepository;
+import jakarta.jms.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +17,23 @@ import java.util.Optional;
 public class ProductService {
 
     final ProductRepository productRepository;
+    final JmsTemplate jmsTemplate;
 
     public Product save( Product product ) {
-        productRepository.save(product);
+        jmsTemplate.convertAndSend("q1", product);
         return product;
+    }
+
+    // listener
+    @JmsListener(containerFactory = "productContainer", destination = "q1")
+    public void productListener(Product product, Message message) throws Exception {
+        System.out.println("Product Save - " + message.getJMSMessageID() );
+        productRepository.save(product);
+        try {
+            Thread.sleep(500);
+        }catch (Exception ex){
+
+        }
     }
 
     public List<Product> list() {
