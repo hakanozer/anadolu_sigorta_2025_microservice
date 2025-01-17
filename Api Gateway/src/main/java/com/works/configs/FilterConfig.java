950feaf35a9f17a9911312036cf6c80ff5e617fe
Hakan ÖZER <hakanozer02@gmail.com>
2025-01-17 15:16:24 +0300

    Update FilterConfig.java

--- Api Gateway/src/main/java/com/works/configs/FilterConfig.java
+++ Api Gateway/src/main/java/com/works/configs/FilterConfig.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
 
@@ -20,6 +22,15 @@ public class FilterConfig implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = "";
+        String roles = "";
+        String url = request.getRequestURI();
+        if (auth != null) {
+            username = auth.getName();
+            roles = auth.getAuthorities().toString();
+        }
+        System.out.println(url + " - " + username + " - " + roles);
         String spanId = tracer.currentSpan().context().spanId();
         String traceId = tracer.currentSpan().context().traceId();
         response.setHeader("spanId", spanId);
